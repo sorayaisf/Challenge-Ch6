e9b615761de44ddb72269a25003d77c6ef8423fb
Soraya Isfandiari <soraya.isf@gmail.com>
2022-03-07 15:10:30 +0700

    add challenge 5

---
+++ app.js
@@ -0,0 +1,113 @@
+//Load the things we need
+const express = require('express');
+const app = express();
+const port = 8080
+const morgan = require('morgan');
+const users = require('./public/db/users.json')
+
+//Set the view engine to ejs
+app.set('view engine', 'ejs');
+
+//Express
+app.use(express.static('public'))
+
+//Logger Middleware
+app.use(morgan('tiny'));
+
+
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: false
+}))
+
+// use res.render to load up an ejs view file //
+
+// Index or Home Page
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: "Home Page",
+    });
+});
+
+// Users Page
+app.get('/users', (req, res) => {
+    console.log(users)
+    res.status(200).json(users)
+});
+
+
+//Sign Up Page
+app.get('/signup', (req, res) => {
+    res.render('signup', {
+        title: "Sign Up Page",
+    })
+})
+
+//Playing Game
+app.get('/game', (req, res) => {
+    res.render('game', {
+
+    })
+})
+
+// Login Page
+app.get('/login', (req, res) => {
+    res.render('login/index', {
+        title: "Login Page",
+    });
+});
+
+//Sign Up to Login Page
+app.get('/signup', (req, res) => {
+    res.render('login/index', {
+        title: "Login Page",
+    });
+});
+
+app.post('/signup', (req, res) => {
+    const {
+        email,
+        password
+    } = req.body
+
+    const newUser = ({
+        email,
+        password
+    })
+    users.push(newUser)
+    res.status(201).redirect('/login')
+})
+
+
+app.post('/login', (req, res) => {
+    const {
+        email,
+        password
+    } = req.body
+    for (user of users) {
+        if (user.email === email && user.password === password) {
+            return res.redirect('game')
+        }
+    }
+    res.status(400).render('alert')
+})
+
+//Play Now
+app.get('/playnow', (req, res) => {
+    res.render('/login'), {
+
+    }
+})
+
+//404 Page
+app.use('/', (req, res) => {
+    res.status(404);
+    res.send('<h1>404</h1>');
+});
+
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
---
+++ public/js/game.js
@@ -0,0 +1,128 @@
+// PLAYER 1
+const userBatu = document.getElementById("userBatu");
+const userKertas = document.getElementById("userKertas");
+const userGunting = document.getElementById("userGunting");
+
+// RESULT
+const vs = document.querySelector(".vs");
+const resultText = document.querySelector(".result-text");
+
+// COMPUTER
+const comBatu = document.getElementById("comBatu");
+const comKertas = document.getElementById("comKertas");
+const comGunting = document.getElementById("comGunting");
+
+// REFRESH
+const refreshPage = document.getElementById("refresh-page");
+
+
+const hands = document.querySelectorAll(".hand");
+
+class Game {
+    constructor() {
+        const userBatu = document.getElementById("userBatu");
+        const userKertas = document.getElementById("userKertas");
+        const userGunting = document.getElementById("userGunting");
+
+    }
+
+    remove() {
+        console.log("remove class");
+        console.log(userKertas);
+
+        userKertas.classList.remove('hightlight');
+        console.log(userKertas);
+    }
+}
+const removeAll = () => {
+    hands.forEach((element) => {
+        element.classList.remove("highlight");
+    });
+};
+
+
+const game = new Game();
+
+const computer = () => {
+    const comRandom = [comBatu, comKertas, comGunting];
+    const random = comRandom[Math.floor(Math.random() * 3)];
+    random.classList.add("highlight");
+    console.log("random :" + random.id);
+
+    return random.id
+};
+
+
+userKertas.onclick = () => {
+    let resultPlayer = userKertas.id;
+    console.log("kertas di klik");
+    removeAll();
+    userKertas.classList.add("highlight");
+    let resultCom = computer();
+    document.querySelector("#game-result .player-result").style.display = "block";
+    document.querySelector("#game-result .result-text").remove();
+    document.querySelector("#game-result .player-result").textContent =
+        getResult(resultCom, resultPlayer);
+}
+
+userBatu.onclick = () => {
+    let resultPlayer = userBatu.id;
+    console.log("batu di klik");
+    removeAll();
+    userBatu.classList.add("highlight");
+    let resultCom = computer();
+    document.querySelector("#game-result .player-result").style.display = "block";
+    document.querySelector("#game-result .result-text").remove();
+    document.querySelector("#game-result .player-result").textContent =
+        getResult(resultCom, resultPlayer);
+};
+
+userGunting.onclick = () => {
+    let resultPlayer = userGunting.id;
+    console.log("gunting di klik");
+    removeAll();
+    userGunting.classList.add("highlight");
+    let resultCom = computer();
+    document.querySelector("#game-result .player-result").style.display = "block";
+    document.querySelector("#game-result .result-text").remove();
+    document.querySelector("#game-result .player-result").textContent =
+        getResult(resultCom, resultPlayer);
+};
+
+refreshPage.onclick = () => {
+    console.log('refresh');
+    document.location.reload(true);
+}
+
+
+function getResult(resultCom, resultPlayer) {
+    let result = "";
+
+
+    // perhitungan
+
+    console.log("resultCom :" + resultCom);
+    console.log("resultPlayer :" + resultPlayer);
+
+
+    if (
+        (resultCom == "comBatu" && resultPlayer == "userKertas") ||
+        (resultCom == "comKertas" && resultPlayer == "userGunting") ||
+        (resultCom == "comGunting" && resultPlayer == "userBatu")
+    ) {
+        result = "PLAYER 1 WIN";
+    } else if (
+        (resultCom == "comGunting" && resultPlayer == "userKertas") ||
+        (resultCom == "comBatu" && resultPlayer == "userGunting") ||
+        (resultCom == "comKertas" && resultPlayer == "userBatu")
+    ) {
+        result = "COM WIN";
+    } else if (
+        (resultCom == "comKertas" && resultPlayer == "userKertas") ||
+        (resultCom == "comGunting" && resultPlayer == "userGunting") ||
+        (resultCom == "comBatu" && resultPlayer == "userBatu")
+    ) {
+        result = "DRAW"
+    }
+    return result;
+}
